0fe85fea7dbe14d2fd3fa027e13b593d77016aed
Aziz <azizatamuradov1994@gmail.com>
2025-11-03 10:42:38 -0500

    Nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
