024c0fa53aa9e679d633fa173e22089afcbddd3b
Aziz <azizatamuradov1994@gmail.com>
2025-11-10 08:04:31 -0500

    Contact component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonails from "./components/Testimonails";
@@ -9,7 +10,8 @@ const App = () => {
       <Header />
       <About />
       <Projects />
-      <Testimonails/>
+      <Testimonails />
+      <Contact/>
     </div>
   );
 };
