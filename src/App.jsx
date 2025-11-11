45a7fd4d5dfbab3a90432327d20c3f99458b09bd
Aziz <azizatamuradov1994@gmail.com>
2025-11-11 08:52:52 -0500

    footer component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonails from "./components/Testimonails";
@@ -15,6 +16,7 @@ const App = () => {
       <Projects />
       <Testimonails />
       <Contact />
+      <Footer/>
     </div>
   );
 };
