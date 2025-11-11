7aa04282ceaf7f025285376ccf1c43944bf37b29
Aziz <azizatamuradov1994@gmail.com>
2025-11-11 07:57:00 -0500

    contact componend finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonails from "./components/Testimonails";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
       <Testimonails />
-      <Contact/>
+      <Contact />
     </div>
   );
 };
