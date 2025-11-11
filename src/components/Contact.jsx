7df34fd4504c40a30208f121ea0b934766057095
Aziz <azizatamuradov1994@gmail.com>
2025-11-11 07:44:34 -0500

    form added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", import.meta.env.VITE_WEB3_KEY);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      alert("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message);
+      setResult();
+    }
+  };
+
   return (
     <div
       className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
@@ -16,7 +43,10 @@ const Contact = () => {
         Ready to Make a Move? Let's Build Your Future Together
       </p>
 
-      <form className="max-w-2xl mx-auto text-gray-600 pt-8">
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text-gray-600 pt-8"
+      >
         <div className="flex flex-wrap">
           <div className="w-full md:w-1/2 text-left">
             Your Name
@@ -38,12 +68,19 @@ const Contact = () => {
               required
             />
           </div>
-              </div>
-              <div className="my-6 text-left">
-                  Message
-                  <textarea className="w-full border border-gary-300 rounded py-3 px-4 mt-2 h-48 resize-none" name="Message" placeholder="Message" required></textarea>
-              </div>
-              <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">Send Message</button>
+        </div>
+        <div className="my-6 text-left">
+          Message
+          <textarea
+            className="w-full border border-gary-300 rounded py-3 px-4 mt-2 h-48 resize-none"
+            name="Message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </div>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
+          {result ? result : "Send Message"}
+        </button>
       </form>
     </div>
   );
